6cb36010009a32bfc5709b98621fc1695b9c9697
Greg Taylor <gtaylor@duointeractive.com>
2011-04-28 10:57:07 -0400

    Account for random S3ResponseError exceptions seen when hitting S3 so frequently. Just skip the key and a later execution will take care of parsing the missed key.

--- tamarin/log_puller.py
+++ tamarin/log_puller.py
@@ -1,4 +1,5 @@
 import boto
+from boto.exception import S3ResponseError
 from django.conf import settings
 from tamarin.models import S3LoggedBucket, S3LogRecord
 from tamarin.parser import S3LogParser
@@ -7,13 +8,26 @@ def pull_and_parse_logs():
     purge_parsed_keys = getattr(settings, 'TAMARIN_PURGE_PARSED_KEYS', False)
     logged_buckets = S3LoggedBucket.objects.get_log_buckets_to_monitor()
     conn = boto.connect_s3(settings.AWS_ACCESS_KEY_ID,
-                               settings.AWS_SECRET_ACCESS_KEY)
+                           settings.AWS_SECRET_ACCESS_KEY)
+
     for logged_bucket in logged_buckets:
         bucket = conn.get_bucket(logged_bucket.log_bucket_name)
         for key in bucket.get_all_keys():
             print key
-            log_contents = key.get_contents_as_string()
+
+            try:
+                log_contents = key.get_contents_as_string()
+            except S3ResponseError:
+                # We're going to get a response error from time to time, S3
+                # is not bullet proof. In this case, move on to the next
+                # key and a later execution of the puller will grab this one.
+                print "S3ResponseError encountered when retrieving: %s" % key
+                continue
+
             parser = S3LogParser(log_contents)
             parser.parse_and_store()
+
             if purge_parsed_keys:
+                # Delete the key after parsing it if the
+                # TAMARIN_PURGE_PARSED_KEYS setting is True. 
                 key.delete()
