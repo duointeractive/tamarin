dda0361b87248a7f26935107bbdf76c7a997cd44
Greg Taylor <gtaylor@duointeractive.com>
2011-04-28 18:35:59 -0400

    Last bit of pylinting.

--- tamarin/management/commands/tamarin_pull_logs.py
+++ tamarin/management/commands/tamarin_pull_logs.py
@@ -1,11 +1,18 @@
+"""
+The tamarin_pull_logs management command may be used in conjunction with
+cron or another task scheduler if you don't have or want to use celery.
+"""
 from django.core.management.base import BaseCommand
 from tamarin.log_puller import pull_and_parse_logs
 
 class Command(BaseCommand):
+    """
+    Nothing magical here, just using the log puller.
+    """
     help = "Looks through S3 logs for broadcast media to find the hogs."
 
-    host_freqs = {}
-    file_freqs = {}
-
     def handle(self, *args, **options):
+        """
+        Call the log pulling function and do work silently.
+        """
         pull_and_parse_logs()
